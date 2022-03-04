859839b339b690370e94546863529947bbb85f42
mahmoud@gmail.com <you@example.com>
2022-03-04 13:51:49 +0200

    v2-with remove

--- src/com/company/AvlTree.java
+++ src/com/company/AvlTree.java
@@ -1,40 +1,40 @@
 package com.company;
 
-public class AvlTree {
-    private static class Node {
-        private final String word;
+public class AvlTree<AnyType extends Comparable<AnyType>> {
+
+    private static class Node<AnyType> {
+        private AnyType value;
         private int height;
-        private Node LeftChild;
-        private Node RightChild;
+        private Node<AnyType> LeftChild;
+        private Node<AnyType> RightChild;
 
-        public Node(String word) {
-            this.word = word;
+        public Node(AnyType value) {
+            this.value = value;
         }
     }
 
-    private Node root;
+    private Node<AnyType> root;
 
-    public void insert_nodes(String word) {
-        root = insert_nodes(root, word);
+    public void insert_nodes(AnyType value) {
+        root = insert_nodes(root, value);
     }
 
-    private Node insert_nodes(Node root, String word) {
+    private Node<AnyType> insert_nodes(Node<AnyType> root, AnyType value) {
         if (root == null) {
-            return new Node(word);
+            return new Node<>(value);
         }
-        if (word.compareTo(root.word) < 0) {
-            root.LeftChild = insert_nodes(root.LeftChild, word);
-        } else if (word.compareTo(root.word) > 0) {
-            root.RightChild = insert_nodes(root.RightChild, word);
+        if (value.compareTo(root.value) < 0) {
+            root.LeftChild = insert_nodes(root.LeftChild, value);
+        } else if (value.compareTo(root.value) > 0) {
+            root.RightChild = insert_nodes(root.RightChild, value);
         } else {
-            System.out.println("Word already exists");
+            System.out.println("value already exists");
         }
         setHeight(root);
-
         return balance(root);
     }
 
-    private int NodeHeight(Node node) {
+    private int NodeHeight(Node<AnyType> node) {
         return (node == null ? -1 : node.height);
     }
 
@@ -42,7 +42,7 @@ public class AvlTree {
         return root.height;
     }
 
-    private Node balance(Node root) {
+    private Node<AnyType> balance(Node<AnyType> root) {
         if (IsLeftHeavy(root)) {
             if (BalanceFactor(root.LeftChild) < 0)
                 root.LeftChild = RotateLeft(root.LeftChild);
@@ -55,50 +55,97 @@ public class AvlTree {
         return root;
     }
 
-    private Node RotateLeft(Node root) {
+    private Node<AnyType> RotateLeft(Node<AnyType> root) {
         var NewRoot = root.RightChild;
         root.RightChild = NewRoot.LeftChild;
         NewRoot.LeftChild = root;
-        setHeight(NewRoot);
         setHeight(root);
+        setHeight(NewRoot);
         return NewRoot;
     }
 
-    private Node RotateRight(Node root) {
+    private Node<AnyType> RotateRight(Node<AnyType> root) {
         var NewRoot = root.LeftChild;
         root.LeftChild = NewRoot.RightChild;
         NewRoot.RightChild = root;
-        setHeight(NewRoot);
         setHeight(root);
+        setHeight(NewRoot);
         return NewRoot;
     }
 
-    private void setHeight(Node node) {
-        node.height = Math.max(NodeHeight(root.LeftChild), NodeHeight(root.RightChild)) + 1;
+    private void setHeight(Node<AnyType> node) {
+        node.height = Math.max(NodeHeight(node.LeftChild), NodeHeight(node.RightChild)) + 1;
     }
 
-    private boolean IsLeftHeavy(Node node) {
+    private boolean IsLeftHeavy(Node<AnyType> node) {
         return BalanceFactor(node) > 1;
     }
 
-    private boolean IsRightHeavy(Node node) {
+    private boolean IsRightHeavy(Node<AnyType> node) {
         return BalanceFactor(node) < -1;
     }
 
-    private int BalanceFactor(Node node) {
+    private int BalanceFactor(Node<AnyType> node) {
         return (node == null ? 0 : NodeHeight(node.LeftChild) - NodeHeight(node.RightChild));
     }
 
-    public boolean search(String word) {
+    public boolean search(AnyType value) {
         var current = root;
         while (current != null) {
-            if (word.compareTo(current.word) < 0)
+            if (value.compareTo(current.value) < 0)
                 current = current.LeftChild;
-            else if (word.compareTo(current.word) > 0)
+            else if (value.compareTo(current.value) > 0)
                 current = current.RightChild;
             else
                 return true;
         }
         return false;
     }
+
+    public Node<AnyType> remove(AnyType value){
+        return remove(root, value); 
+    }
+    
+    private Node<AnyType> remove(Node<AnyType> root, AnyType value){
+        if(root == null)
+            return root;
+        int x = value.compareTo(root.value);
+        if(x < 0)
+            root.LeftChild = remove(root.LeftChild, value);
+        else if(x > 0)
+            root.RightChild = remove(root.RightChild, value);
+        else if(root.RightChild != null && root.LeftChild != null){
+            var t = findMin(root.RightChild);
+            root.value = t.value;
+            root.RightChild = remove(root.RightChild, t.value);
+        }
+        else{
+            root = (root.LeftChild != null) ? root.LeftChild : root.RightChild;
+        }
+        return balance(root);
+    }
+
+    private Node<AnyType> findMin(Node<AnyType> root){
+        if(root.LeftChild == null)
+                return root;
+        return findMin(root.LeftChild);
+    }
+
+    private void printInorder(Node<AnyType> node)
+    {
+        if (node == null)
+            return;
+
+        /* first recur on left child */
+        printInorder(node.LeftChild);
+
+        /* then print the data of node */
+        System.out.print(node.value + " ");
+
+        /* now recur on right child */
+        printInorder(node.RightChild);
+    }
+
+    public void printInorder() { printInorder(root); }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        AvlTree Avl = new AvlTree();
-        Avl.insert_nodes("1");
-        Avl.insert_nodes("3");
-        Avl.insert_nodes("2");
-
-        System.out.println(Avl.TreeHeight());
+        AvlTree<String> tree = new AvlTree<>();
+        tree.insert_nodes("a");
+        tree.insert_nodes("f");
+        tree.insert_nodes("g");
+        tree.insert_nodes("b");
+        tree.insert_nodes("q");
+        tree.insert_nodes("r");
+        tree.insert_nodes("c");
+        tree.remove("c");
+        tree.remove("q");
+        tree.printInorder();
+        System.out.println("Height: "+tree.TreeHeight());
     }
 }
